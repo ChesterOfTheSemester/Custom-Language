84fb9ca1d6b93e95061d3de73c29d5f9e4c157ef
Chester Abrahams <43809102+ChesterOfTheSemester@users.noreply.github.com>
2021-06-06 13:23:35 +0100

    Create example.s

---
+++ example.s
@@ -0,0 +1,26 @@
+// Custom language progress
+// Custom compiler/assembler
+// Mix C-like script with Assembly
+// Name yet to be decided. I was thinking of Chester++, but nah
+
+// Starts to execute code from line 1
+#include <stdio.h>
+
+MOV RDI, 0x01;
+MOV RSI, "Begin\n"; // Mix escaped string literal
+MOV RDX, 6;
+
+int 0x80; // Syscall
+register rtn= EAX;
+
+if (rtn) jmp main; // goto and gosub are optional, use assembly
+else ret;
+
+// Main function/label is optional
+// Commandline are still parsed into main parameters
+main(int argv, char **argc) 
+{
+  printf("End\n");
+  MOV RAX, 0; // Automatically enable 64-bit
+  return 0;
+}
