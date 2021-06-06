eb8efe43bd340d5223bd64b575ffacc2ad7674cf
Chester Abrahams <43809102+ChesterOfTheSemester@users.noreply.github.com>
2021-06-06 13:35:20 +0100

    Update example.s

--- example.s
+++ example.s
@@ -11,14 +11,14 @@ MOV RSI, "Begin\n"; // Mix escaped string literal
 MOV RDX, 6;
 
 int 0x80; // Syscall sys_write or _write by matching lookup table
-register rtn= EAX;
+register rtn = EAX;
 
 if (rtn) jmp main; // goto and gosub are optional, use assembly
 else ret;
 
 // Main function/label is optional
 // Commandline are still parsed into main parameters
-main(int argv, char **argc) 
+main(int argc, char **argb) 
 {
   printf("End\n");
   MOV RAX, 0; // Automatically enable 64-bit
